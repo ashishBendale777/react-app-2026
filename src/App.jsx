28108f60e4f1af52dc4830b9aac1fea7b4f3bcb9
technoweit77 <technoweit77@gmail.com>
2026-03-04 10:45:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/Home'
+import "./css/Home.css"
+const App = () => {
+  return (
+    <div>
+      <h1>Hi From App</h1>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
