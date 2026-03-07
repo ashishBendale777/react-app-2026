d9fe2eb99e17215e6b361984b47adaf7edac29b7
technoweit77 <technoweit77@gmail.com>
2026-03-07 10:11:25 +0530

    use state hook implimented

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,33 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  //define state variable
+  const [counter, setCounter] = useState(0)
+
+  const [show, setshow] = useState(false)
+
+  let increase = () => {
+    setCounter(counter + 1)
+  }
+  let decrease = () => {
+    setCounter(counter - 1)
+
+  }
+
   return (
     <>
-      <Typography variant='h4'>Contact US</Typography>
+      {show && <Typography variant='h4'>Contact US</Typography>}
+      <Typography variant='h6'>{counter}</Typography>
+
+      <Button onClick={() => decrease()} variant='outlined'>-</Button>
+      <Button onClick={() => increase()} variant='outlined'>+</Button>
+
+
+      <Button onClick={() => {
+        setshow(!show)
+      }}>{show? "Hide":"Show"}</Button>
     </>
   )
 }
