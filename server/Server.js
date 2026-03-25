b3bf30833c3597b9214fc3ccbb03495611450595
technoweit77 <technoweit77@gmail.com>
2026-03-25 10:21:57 +0530

    create and fetch done

--- server/Server.js
+++ server/Server.js
@@ -1,9 +1,12 @@
 import express from "express"
 import connectToDb from "./src/config/dbconfig.js"
 import { userRoutes } from "./src/routes/UsserRoutes.js"
+import bodyParser from "body-parser"
 
 //create server
 const Server = express()
+
+Server.use(bodyParser.json())
 
 Server.get("/", (req, res) => {
     res.send("Hello Fron Server")
--- server/src/controller/UserController.js
+++ server/src/controller/UserController.js
@@ -1,12 +1,43 @@
 import { User } from "../models/UserSchema.js"
 
-User
-
 ///create user
-let createUser = (req, res) => { }
+let createUser = async (req, res) => {
+    let body = req.body
+    console.log(body)
+    try {
+        let result = await User.create(body)
+        res.status(200).json({
+            message: "User Create Successfully.",
+            data: result,
+            success: true
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            data: null,
+            success: false
+        })
+    }
+}
 
 //find all users
-let fetchAllUsers = (req, res) => { }
+let fetchAllUsers = async (req, res) => {
+
+    try {
+        let result  = await User.find()
+        res.status(200).json({
+            message: "User Fetched Successfully.",
+            data: result,
+            success: true
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            data: null,
+            success: false
+        })
+    }
+}
 
 
 //find user by ID
