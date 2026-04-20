47bc6dde81c16092dd9faafe031d995f37371b4d
technoweit77 <technoweit77@gmail.com>
2026-04-20 10:27:27 +0530

    done MRT

--- src/pages/admin/AdminOrders.jsx
+++ src/pages/admin/AdminOrders.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import axios from 'axios'
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table'
+import React, { useEffect, useMemo, useState } from 'react'
 
 const AdminOrders = () => {
+
+  const [allOrders, setallOrders] = useState([])
+  useEffect(() => {
+    let fetchAllOrders = async () => {
+      try {
+        let { data } = await axios.get("")
+        setallOrders(data?.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    //function call
+    fetchAllOrders()
+  }, [])
+
+
+  let columns = useMemo(() => [
+    {
+      accessorKey: "userId.firstName",
+      header: "Customer Name"
+    },
+
+    {
+      accessorKey: "userId.address.city",
+      header: "City"
+    },
+
+    {
+      accessorKey: "orderAmount",
+      header: "Amount"
+    },
+    {
+      accessorKey: "orderStatus",
+      header: "Status"
+    },
+
+    {
+      accessorKey: "userId.email",
+      header: "Email"
+    },
+    {
+      header: "Actions",
+      cell: ({ cell }) => <Button variant='contained'>Details</Button>,
+    }
+  ], [])
+
+
+  let table = useMaterialReactTable({
+    columns,
+    data: allOrders,
+    enablePagination: true,
+
+  })
   return (
-    <div>AdminOrders</div>
+    <>
+      <Typography variant='h4'>All orders</Typography>
+      <Box>
+        <MaterialReactTable table={table} />
+      </Box>
+    </>
   )
 }
 
